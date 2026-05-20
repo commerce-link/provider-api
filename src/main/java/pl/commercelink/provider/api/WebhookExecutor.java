6e4325abea78fdfd950a18dd6402f0950c4fbfb5
ptrepka <przemyslaw.trepka@pragmaticcoders.com>
2026-05-20 10:18:13 +0200

    Update version to 0.1.3 and refactor WebhookExecutor and WebhookBinding to simplify event handling

--- src/main/java/pl/commercelink/provider/api/WebhookExecutor.java
+++ src/main/java/pl/commercelink/provider/api/WebhookExecutor.java
@@ -1,6 +1,6 @@
 package pl.commercelink.provider.api;
 
 @FunctionalInterface
-public interface WebhookExecutor<T, R> {
-    R execute(T event, WebhookContext context);
+public interface WebhookExecutor<R> {
+    R execute(String payload, WebhookContext context);
 }
